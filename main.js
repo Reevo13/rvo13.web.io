77504e2cbf10573ed1a58118bf1f7d9e613fe730
Reevo13 <121067601+Reevo13@users.noreply.github.com>
2023-03-04 03:12:50 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,148 @@
+const body = document.body
+const pp = document.getElementById('pp')
+const fig = document.getElementById('fig')
+const fwa = document.getElementById('fwa')
+const dm = document.getElementById('dm')
+const rb = document.getElementById('rb')
+const pst = document.getElementById('pst')
+const beli = prompt('Ketik "Y" Untuk Masuk Web')
+ if (beli === "Y" || beli === "Y") {
+   alert("Anda Berhasil Memasuki Website")
+   
+ } else {
+   window.location.href = "http://www.google.com";
+
+ }
+
+//body
+body.style.background = 'black'
+//pp
+pp.style.padding = '0px'
+pp.style.width = '100px'
+pp.style.border = '3px solid'
+pp.style.borderColor = 'gray'
+pp.style.background = ''
+pp.style.borderRadius = '50%'
+pp.style.position = 'relative'
+pp.style.left = '120px'
+pp.style.top = '35px'
+
+//figc
+fig.style.width = '30px'
+fig.style.position = 'relative'
+fig.style.left = '99px'
+fig.style.top = '-40px'
+
+//fwa
+fwa.style.width = '30px'
+fwa.style.position = 'relative'
+fwa.style.left = '99px'
+fwa.style.top = '-40px'
+
+//fyt
+fyt.style.width = '30px'
+fyt.style.position = 'relative'
+fyt.style.left = '99px'
+fyt.style.top = '-40px'
+
+//fyt
+fdc.style.width = '30px'
+fdc.style.position = 'relative'
+fdc.style.left = '99px'
+fdc.style.top = '-40px'
+
+//fgmm
+fgm.style.width = '30px'
+fgm.style.position = 'relative'
+fgm.style.left = '99px'
+fgm.style.top = '-40px'
+
+//dm
+dm.style.width = '30px'
+dm.style.position = 'relative'
+dm.style.left = '75px'
+dm.style.top = '-40px'
+
+//rb
+rb.style.width = '30px'
+rb.style.position = 'relative'
+rb.style.left = '80px'
+rb.style.top = '-40px'
+
+//pst
+pst.style.width = '35px'
+pst.style.position = 'relative'
+pst.style.left = '77px'
+pst.style.top = '-45px'
+
+//jki
+jki.style.width = '30px'
+jki.style.position = 'relative'
+jki.style.left = '85px'
+jki.style.top = '-40px'
+
+//jwb
+jwb.style.width = '30px'
+jwb.style.position = 'relative'
+jwb.style.left = '85px'
+jwb.style.top = '-40px'
+
+//jwb
+ml.style.width = '50px'
+ml.style.position = 'relative'
+ml.style.left = '85px'
+ml.style.top = '-40px'
+
+//ig
+/*ig.style.color = 'white'
+ig.style.position = 'relative'
+ig.style.right = ''
+ig.style.top = '10px'
+ig.style.hight = '0px'
+*/ 
+
+function contac() {
+  contak.style.display = 'inline'
+  btn.style.display = 'none'
+}
+
+function back() {
+  contak.style.display = 'none'
+  btn.style.display = 'inline'
+  kk.style.display = 'none'
+  store.style.display = 'none'
+}
+
+function dd() {
+  kk.style.display = 'inline'
+ btn.style.display = 'none'
+}
+
+function str() {
+  store.style.display = 'inline'
+  btn.style.display = 'none'
+}
+
+function igr () {
+     window.location.href = "https://www.instagram.com/reevo888/";
+}
+
+function wa() {
+  window.location.href = "https://wa.me/6289630190781"
+}
+
+function yt() {
+  window.location.href = "https://youtube.com/@r33v09"
+}
+
+function tt() {
+  window.location.href = "https://discord.gg/wjmhKp99ER"
+}
+
+function gm() {
+  alert('comming soon')
+}
+
+function tp() {
+  window.location.href = "https://codashop.com"
+}
